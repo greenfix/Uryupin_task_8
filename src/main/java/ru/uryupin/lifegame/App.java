2c019c40e678675da1f9de831dcc9e530c7b1ff6
Serg <sayrosstes@gmail.com>
2019-11-11 09:23:27 +0300

    Init

---
+++ src/main/java/ru/uryupin/lifegame/App.java
@@ -0,0 +1,13 @@
+package ru.uryupin.lifegame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
